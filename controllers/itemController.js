e0091f2f50b8ff43934b3b6c25098c8b59ba719b
Raiyan109 <kraiyan109@gmail.com>
2023-03-04 15:52:46 +0600

    vercel

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,22 @@
 const Item = require('../models/itemModel.js');
 const multer = require('multer');
+const fs = require('fs')
+const path = require('path')
+
+
+// storage
+const Storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({
+    storage: Storage
+}).single('image')
 
 // get all items
 const getItems = async (req, res) => {
@@ -18,16 +35,41 @@ const getItem = async (req, res) => {
 }
 // Post items
 const createItem = async (req, res) => {
-    const { name, price, des, supplier, quantity } = req.body
+    const { name, price, des, supplier, quantity, imageUrl } = req.body
 
     // add doc to db
     try {
-        const item = await Item.create({ name, price, des, supplier, quantity })
+        const item = await Item.create({ name, price, des, supplier, quantity, image: imageUrl })
         res.status(200).json(item)
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
+    // upload(req, res, async (err) => {
+    //     if (err) {
+    //         return res.status(400).json({ error: err.message })
+    //     }
+    //     const { name, price, des, supplier, quantity } = req.body
+    //     if (!req.file) {
+    //         return res.status(400).json({ error: 'No file uploaded' })
+    //     }
+
+    //     // Access filename property of the uploaded file
+    //     const filename = req.file.filename
+    //     const image = {
+    //         data: fs.readFileSync(path.join(__dirname, '../uploads' + req.file.filename)),
+    //         contentType: 'image/png'
+    //     }
+    //     // add doc to db
+    //     try {
+    //         const item = await Item.create({ image: { data: req.file.buffer, contentType: req.file.mimetype }, name, price, des, supplier, quantity })
+    //         res.status(200).json(item)
+    //     } catch (error) {
+    //         res.status(400).json({ error: error.message })
+    //     }
+    // })
 }
+
+
 // Delete a item
 const deleteItem = async (req, res) => {
     const { id } = req.params
